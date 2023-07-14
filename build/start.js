d699ee82fb45bab09c819a1c50f865aebb1b5b03
777freezy <joaovitor@iMac-Pro.local>
2023-07-14 15:00:58 -0300

    function post and scheduling of post is work

--- build/start.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.threadsAPI = void 0;
-var threads_api_1 = require("threads-api");
-exports.threadsAPI = new threads_api_1.ThreadsAPI({
-    username: 'user do Threads',
-    password: 'senha da conta',
-});
